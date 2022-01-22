c31139e02503a67ca32c8992ca1bdbe890f5c443
Langrizkie <langrizkie@gmail.com>
2022-01-23 05:19:16 +0700

    feat: fix rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'wdio'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'error',
+    '@typescript-eslint/no-unused-vars':  process.env.NODE_ENV === 'production' ? 'off' : 'error'
   }
 }
